13a21235cbf5e9e4869008b0a391211841db4bae
cjqw <2462457751@qq.com>
2017-03-29 10:48:35 +0800

    finish data parser

--- src/core.py
+++ src/core.py
@@ -3,6 +3,20 @@
 from liblinearutil import *
 from tools.readdata import *
 from tools.util import *
+from random import shuffle
+
+def getlabel(x):
+    label = x[0][0]
+    if label[0] == 'A':
+        return 1
+    else:
+        return -1
 
 data = read_data()
-print(data[0])
+# print(data[2])
+shuffle(data)
+y = mapv(getlabel,data)
+x = mapv(lambda x: x[1],data)
+m = train(y[:200], x[:200], '-c 4')
+# print(x,y)
+p_label, p_acc, p_val = predict(y[200:], x[200:], m)
--- src/tools/readdata.py
+++ src/tools/readdata.py
@@ -2,22 +2,24 @@ from tools.util import *
 
 def parse_item(item):
     x,y = item.split(":")
-    return [int(x),float(y)]
+    return {int(x):float(y)}
 
 def parse_param(param):
-    # m = {}
-    # for item in param:
-    #     m.set(1,1)
-    # return m
-    return mapv(parse_item,param)
+    m = {}
+    for item in param:
+        m.update(parse_item(item))
+    return m
+
+def parse_label(labels):
+    return mapv(identity(),labels.split(','))
 
 def parse_lines(data_line):
     item = data_line[:-1].split(" ")
-    return [item[0],parse_param(item[1:])]
+    return [parse_label(item[0]),parse_param(item[1:])]
 
 def read_data():
     input_file = "data/train.txt"
     with open(input_file , "r")  as fin:
         data = fin.readlines()
-    data = data[:10]
+    # data = data[:10]
     return mapv(parse_lines,data)
