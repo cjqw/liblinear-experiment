789145d0789522dcbe7160b1625231d184781d46
cjqw <2462457751@qq.com>
2017-04-05 11:17:55 +0800

    use getValue function

--- src/core/minmax.py
+++ src/core/minmax.py
@@ -3,13 +3,13 @@ from utils.util import *
 from random import randint
 
 def getRandomClass(m):
-    return randint(1,MAX_CLASS)-1
+    return randint(1,MAX_CLASS) - 1
 
 def runMinMaxTest(data,test):
     data = partition(data,getRandomClass)
     print("Begin to get min-max model...")
-    # # m = getModel(data, '-c 4','Brute.model')
-    # print("Begin to test min-max algorithm...")
-    # p_label, p_acc, p_val = predictResult(test, m)
-    # return {"label":p_label,
-    #         "acc":p_acc}
+    # m = getModel(data, '-c 4','Brute.model')
+    print("Begin to test min-max algorithm...")
+    p_label, p_acc, p_val = predictResult(test, m)
+    return {"label":p_label,
+            "acc":p_acc}
