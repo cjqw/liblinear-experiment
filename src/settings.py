62546d736601a07240e5789ea66b996efd6ec301
cjqw <2462457751@qq.com>
2017-04-09 18:11:55 +0800

    add commit for settings & move memorize into settings

--- src/settings.py
+++ src/settings.py
@@ -1,8 +1,20 @@
+# The file name of trainning data
 TRAIN_DATA_SET = 'train.txt'
+# The file name of test data
 TEST_DATA_SET = 'test.txt'
+# The path to store .model files
 MODEL_PATH = 'models/'
+# The path to store input files
 DATA_PATH = 'data/'
 
+# Max quantity of input & test data items
+# Set to small number for debug
+# Set it to 0 or below to forbid dropping data
 TRAIN_DATA = 0
 
+# Maximum class when training Min-Max models with
+# randomized partition function
 MAX_CLASS = 3
+
+# Whether trust the post models or not
+MEMORIZE = True
--- src/tools/tools.py
+++ src/tools/tools.py
@@ -2,18 +2,20 @@ from liblinearutil import *
 from settings import *
 from utils.util import *
 
-def getModel(data,cmd,name = "MODEL", memorize = True):
+def getModel(data,cmd,name = "MODEL"):
     y = mapv(getValue("sign"),data)
     x = mapv(getValue("param"),data)
-    if not memorize:
-        return train(y,x,cmd)
+
+    if not MEMORIZE:
+        model = train(y,x,cmd)
     else:
         with cd(MODEL_PATH):
             model = load_model(name)
             if model == None:
                 model = train(y,x,cmd)
-                save_model(name,model)
-        return model
+
+    save_model(name,model)
+    return model
 
 def predictResult(data ,model):
     y = mapv(getValue("sign"),data)
